eceb85b3ce0a4b177b3be592ae90ddb16d0c7719
Dean Crowley <deancrowley1@gmail.com>
2023-11-27 00:36:53 +0000

    README Added

--- seed/movieReviews.ts
+++ seed/movieReviews.ts
@@ -9,6 +9,13 @@ export const movieReviews: MovieReview[] = [
     rating: 4,
   },
   {
+    movieId: 1234,
+    reviewerName: "Katie", 
+    reviewDate: "28-01-2023",      
+    content: "This movie was terrible!",
+    rating: 1,
+  },
+  {
     movieId: 2345,
     reviewerName: "John", 
     reviewDate: "23-02-2023",     
